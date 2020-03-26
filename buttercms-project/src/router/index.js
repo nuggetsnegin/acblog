2ce7ae5cd907d7745ba324ab2de34cb3585876af
Negin Sauermann <negin.sauermann@campbrain.com>
2020-03-26 16:11:25 -0400

    fix routing

--- buttercms-project/src/router/index.js
+++ buttercms-project/src/router/index.js
@@ -1,8 +1,8 @@
 import Vue from "vue";
 import Router from "vue-router";
 import HelloWorld from "@/components/HelloWorld";
-import CustomersHome from "@/components/BlogHome";
-import CustomerPage from "@/components/BlogPage";
+import BlogHome from "@/components/BlogHome";
+import BlogPost from "@/components/BlogPost";
 
 Vue.use(Router);
 
@@ -20,8 +20,8 @@ export default new Router({
     },
     {
       path: "/blog/:slug",
-      name: "blog-page",
-      component: BlogPage
+      name: "blog-post",
+      component: BlogPost
     }
   ]
 });
